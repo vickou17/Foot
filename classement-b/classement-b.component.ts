394204fc8bad538a60ed8e409b4605c9acbe2258
vickou17 <36474682+vickou17@users.noreply.github.com>
2020-01-01 16:56:28 +0100

    Add files via upload

---
+++ classement-b/classement-b.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-classement-b',
+  templateUrl: './classement-b.component.html',
+  styleUrls: ['./classement-b.component.css']
+})
+export class ClassementBComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ classement-c/classement-c.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-classement-c',
+  templateUrl: './classement-c.component.html',
+})
+export class ClassementCComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
